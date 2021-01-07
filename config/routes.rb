e97ddd31c6e6170ca60fcfbeee0964d51c523dcb
Rajan Dalal <rajan.dalal@appfolio.com>
2021-01-06 19:12:20 -0600

    Reroute home page to images index
    Co-authored-by: Andrew Chen <yahn-chung.chen@appfolio.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :images, only: %w[create new show]
+  resources :images, only: %w[create new show index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#home'
+  root 'images#index'
 end
