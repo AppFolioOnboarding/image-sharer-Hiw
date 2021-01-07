3edd1276a87f941530d92a97269aeda54933ab93
Rajan Dalal <rajan.dalal@appfolio.com>
2021-01-07 16:20:47 -0600

    Add tags page and tags linking.
    Co-authored-by: Swaraj Patel <swaraj.patel@appfolio.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :images, only: %w[create new show index]
 
+  get 'images/tags/:tag', to: 'images#tags'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'images#index'
 end
