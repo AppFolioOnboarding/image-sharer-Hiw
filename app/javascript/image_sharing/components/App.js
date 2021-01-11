00b07248474b1e8cfcf04cb9e548f2060ab71bf9
Rajan Dalal <rajan.dalal@appfolio.com>
2021-01-11 12:39:04 -0600

    Added feedbacks page frontend and tests.
    Co-authored-by: Jon Schneider <jon.schneider@appfolio.com>

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,13 +1,44 @@
 import React from 'react';
+import { Button, Container, Form, FormGroup, Label, Input } from 'reactstrap';
 import Header from './Header';
+import Footer from './Footer';
 
-export default function App() {
-  return (
-    <div>
-      <Header title="Tell us what you think" />
-      Put your components here: Flash Message, Form, Footer
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      comments: '',
+    };
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header title="Tell us what you think" />
+        <Form>
+          <FormGroup>
+            <Label for="nameForm">Your Name:</Label>
+            <Input
+              type="text"
+              id="nameForm"
+              value={this.state.name}
+              onChange={e => this.setState({ name: e.target.value })}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="commentsForm">Comments: </Label>
+            <Input
+              type="textarea"
+              id="commentsForm"
+              value={this.state.comments}
+              onChange={e => this.setState({ comments: e.target.value })}
+            />
+          </FormGroup>
+          <Button color="primary">Submit</Button>
+        </Form>
+        <Footer text="Copyright: Appfolio Inc. Onboarding" />
+      </Container>
+    );
+  }
 }
-
-/* TODO: Add Prop Types check*/
--- app/javascript/image_sharing/test/components/App.spec.js
+++ app/javascript/image_sharing/test/components/App.spec.js
@@ -1,13 +1,62 @@
 /* eslint-env mocha */
 
 import assert from 'assert';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import App from '../../components/App';
 
 describe('<App />', () => {
   it('should render correctly', () => {
-    const wrapper = mount(<App />);
-    assert(wrapper.contains('Tell us what you think'));
+    const wrapper = shallow(<App />);
+
+    assert.strictEqual(wrapper.find('Header').length, 1);
+    assert.strictEqual(wrapper.find('Header').prop('title'), 'Tell us what you think');
+    assert.strictEqual(wrapper.find('Button').length, 1);
+    assert.strictEqual(wrapper.find('#nameForm').length, 1);
+    assert.strictEqual(wrapper.find('#commentsForm').length, 1);
+    assert.strictEqual(wrapper.find('Footer').length, 1);
+    assert.strictEqual(wrapper.find('Footer').prop('text'), 'Copyright: Appfolio Inc. Onboarding');
+  });
+
+  it('should be able to populate name', () => {
+    const wrapper = shallow(<App />);
+    const name = 'Foo Bar';
+
+    wrapper.setState({ name });
+    const nameForm = wrapper.find('#nameForm');
+
+    assert.strictEqual(nameForm.prop('value'), name);
+  });
+
+  it('should be able to set name state', () => {
+    const wrapper = shallow(<App />);
+    const name = 'Foo Bar';
+    const event = { target: { value: name } };
+
+    const nameForm = wrapper.find('#nameForm');
+    nameForm.simulate('change', event);
+
+    assert.strictEqual(wrapper.state('name'), name);
+  });
+
+  it('should be able to populate comments', () => {
+    const wrapper = shallow(<App />);
+    const comments = 'Foo Bar\nThis is a comment!';
+
+    wrapper.setState({ comments });
+    const commentsForm = wrapper.find('#commentsForm');
+
+    assert.strictEqual(commentsForm.prop('value'), comments);
+  });
+
+  it('should be able to set comments state', () => {
+    const wrapper = shallow(<App />);
+    const comments = 'Foo Bar\nThis is a comment!';
+    const event = { target: { value: comments } };
+
+    const commentsForm = wrapper.find('#commentsForm');
+    commentsForm.simulate('change', event);
+
+    assert.strictEqual(wrapper.state('comments'), comments);
   });
 });
